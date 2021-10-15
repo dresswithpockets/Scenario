1a2eb1d10b92bb42a827b92f740e8b1017b74209
Ashley <dresswithpockets@pm.me>
2021-10-14 19:56:19 -0700

    add resources for working with moq resources

---
+++ src/Scenario.Moq/MockedResourceDescription.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+
+namespace Scenario.Moq
+{
+    public class MockedResourceDescription<TScenarioBuilder, TMocked> :
+        BaseResourceDescription<TScenarioBuilder, TMocked>
+        where TScenarioBuilder : IScenarioBuilder
+        where TMocked : class
+    {
+        private readonly Mock<TMocked> _mock;
+
+        public MockedResourceDescription(
+            TScenarioBuilder scenarioBuilder,
+            Action<IReadOnlyCollection<TMocked>?>? resultCallback)
+            : base(scenarioBuilder, resultCallback)
+        {
+            _mock = new Mock<TMocked>();
+        }
+        
+        public MockedResourceDescription(
+            TScenarioBuilder scenarioBuilder,
+            Action<IReadOnlyCollection<TMocked>?>? resultCallback,
+            params object[] args)
+            : base(scenarioBuilder, resultCallback)
+        {
+            _mock = new Mock<TMocked>(args);
+        }
+        
+        public MockedResourceDescription(
+            TScenarioBuilder scenarioBuilder,
+            Action<IReadOnlyCollection<TMocked>?>? resultCallback,
+            MockBehavior mockBehavior,
+            params object[] args)
+            : base(scenarioBuilder, resultCallback)
+        {
+            _mock = new Mock<TMocked>(mockBehavior, args);
+        }
+        
+        public MockedResourceDescription(
+            TScenarioBuilder scenarioBuilder,
+            Action<IReadOnlyCollection<TMocked>?>? resultCallback,
+            MockBehavior mockBehavior)
+            : base(scenarioBuilder, resultCallback)
+        {
+            _mock = new Mock<TMocked>(mockBehavior);
+        }
+        
+        public MockedResourceDescription(
+            TScenarioBuilder scenarioBuilder,
+            Action<IReadOnlyCollection<TMocked>?>? resultCallback,
+            Expression<Func<TMocked>> newExpression,
+            MockBehavior behavior = MockBehavior.Default)
+            : base(scenarioBuilder, resultCallback)
+        {
+            _mock = new Mock<TMocked>(newExpression, behavior);
+        }
+
+        protected override Task<IImmutableList<TMocked>> ScopeActionAsync(IServiceScope scope)
+        {
+            var results = new[] { _mock.Object };
+            return Task.FromResult<IImmutableList<TMocked>>(results.ToImmutableList());
+        }
+
+        public MockedResourceDescription<TScenarioBuilder, TMocked> Mock(Action<Mock<TMocked>> mocker)
+        {
+            mocker(_mock);
+            return this;
+        }
+    }
+}
---
+++ src/Scenario.Moq/MockedSubResourceDescription.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+
+namespace Scenario.Moq
+{
+    public class MockedSubResourceDescription<TScenarioBuilder, TDependent, TMocked> :
+        BaseSubResourceDescription<TScenarioBuilder, TDependent, TMocked>
+        where TScenarioBuilder : IScenarioBuilder
+        where TMocked : class
+    {
+        private readonly Mock<TMocked> _mock;
+
+        public MockedSubResourceDescription(
+            IScenarioBuilderResourceDescription<TScenarioBuilder, TDependent> dependentResource,
+            Action<IReadOnlyCollection<TMocked>?>? resultCallback)
+            : base(dependentResource.Builder, dependentResource, resultCallback)
+        {
+            _mock = new Mock<TMocked>();
+        }
+        
+        public MockedSubResourceDescription(
+            IScenarioBuilderResourceDescription<TScenarioBuilder, TDependent> dependentResource,
+            Action<IReadOnlyCollection<TMocked>?>? resultCallback,
+            params object[] args)
+            : base(dependentResource.Builder, dependentResource, resultCallback)
+        {
+            _mock = new Mock<TMocked>(args);
+        }
+        
+        public MockedSubResourceDescription(
+            IScenarioBuilderResourceDescription<TScenarioBuilder, TDependent> dependentResource,
+            Action<IReadOnlyCollection<TMocked>?>? resultCallback,
+            MockBehavior mockBehavior,
+            params object[] args)
+            : base(dependentResource.Builder, dependentResource, resultCallback)
+        {
+            _mock = new Mock<TMocked>(mockBehavior, args);
+        }
+        
+        public MockedSubResourceDescription(
+            IScenarioBuilderResourceDescription<TScenarioBuilder, TDependent> dependentResource,
+            Action<IReadOnlyCollection<TMocked>?>? resultCallback,
+            MockBehavior mockBehavior)
+            : base(dependentResource.Builder, dependentResource, resultCallback)
+        {
+            _mock = new Mock<TMocked>(mockBehavior);
+        }
+        
+        public MockedSubResourceDescription(
+            IScenarioBuilderResourceDescription<TScenarioBuilder, TDependent> dependentResource,
+            Action<IReadOnlyCollection<TMocked>?>? resultCallback,
+            Expression<Func<TMocked>> newExpression,
+            MockBehavior behavior = MockBehavior.Default)
+            : base(dependentResource.Builder, dependentResource, resultCallback)
+        {
+            _mock = new Mock<TMocked>(newExpression, behavior);
+        }
+
+        protected override Task<IImmutableList<TMocked>> ScopeActionAsync(IServiceScope scope)
+        {
+            return Task.FromResult<IImmutableList<TMocked>>(new ImmutableArray<TMocked> { _mock.Object });
+        }
+
+        public MockedSubResourceDescription<TScenarioBuilder, TDependent, TMocked> Mock(Action<Mock<TMocked>> mocker)
+        {
+            mocker(_mock);
+            return this;
+        }
+    }
+}
