b2dab2fea63d0d786f06583949ac7dac8a9d7ae3
Ashley <dresswithpockets@pm.me>
2021-10-14 19:57:02 -0700

    add extension method for calling BuildAsync from resource

---
+++ src/Scenario/ScenarioBuilderExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace Scenario
+{
+    public static class ScenarioBuilderExtensions
+    {
+        public static Task<IScenario> BuildAsync<TScenarioBuilder, TResource>(
+            this IScenarioBuilderResourceDescription<TScenarioBuilder, TResource> resourceDescription)
+            where TScenarioBuilder : IScenarioBuilder
+            => resourceDescription.Builder.BuildAsync();
+    }
+}
