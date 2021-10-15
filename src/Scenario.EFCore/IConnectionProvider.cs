902e33b6f527409b6abc40f20e90d40143b36509
Ashley <dresswithpockets@pm.me>
2021-10-14 22:43:40 -0700

    add sqlite connection provider for DbContext's

---
+++ src/Scenario.EFCore/IConnectionProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace Scenario.EFCore
+{
+    public interface IConnectionProvider<TContext> : IDisposable
+        where TContext : DbContext
+    {
+        DbConnection Connection { get; }
+        
+        DbContextOptions<TContext> Options { get; }
+    }
+}
---
+++ src/Scenario.EFCore/SqliteConnectionProvider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.IO;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace Scenario.EFCore
+{
+    public class SqliteConnectionProvider<TContext> : IConnectionProvider<TContext>
+        where TContext : DbContext
+    {
+        public DbConnection Connection { get; }
+        
+        public DbContextOptions<TContext> Options { get; }
+
+        public SqliteConnectionProvider(Action<SqliteDbContextOptionsBuilder>? sqliteOptionsAction = null)
+        {
+            Options = new DbContextOptionsBuilder<TContext>().UseSqlite(CreateTemporaryDatabase(), sqliteOptionsAction)
+                .Options;
+            Connection = RelationalOptionsExtension.Extract(Options).Connection;
+        }
+
+        private static DbConnection CreateTemporaryDatabase()
+        {
+            var tempDb = Path.GetTempFileName();
+            var connection = new SqliteConnection($"Filename={tempDb}");
+            connection.Open();
+            return connection;
+        }
+
+        public void Dispose() => Connection.Dispose();
+    }
+}
