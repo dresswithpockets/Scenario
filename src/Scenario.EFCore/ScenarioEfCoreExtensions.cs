75c53c95d2c0181a760e54bf261a14d79f393a95
Ashley <dresswithpockets@pm.me>
2021-10-18 05:57:24 -0700

    fix warnings

--- src/Scenario.EFCore/ScenarioEfCoreExtensions.cs
+++ src/Scenario.EFCore/ScenarioEfCoreExtensions.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Scenario.EFCore
