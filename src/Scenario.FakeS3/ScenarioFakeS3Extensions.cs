b094de89b34bcb9be7e78f8cde9a8be233f4da81
Ashley Horton <ashley@echofi.co>
2021-11-19 19:57:24 -0500

    convert projects to use source generator

--- src/Scenario.FakeS3/ScenarioFakeS3Extensions.cs
+++ src/Scenario.FakeS3/ScenarioFakeS3Extensions.cs
@@ -6,34 +6,21 @@ namespace Scenario.FakeS3
 {
     public static class ScenarioFakeS3Extensions
     {
-        public static TScenarioBuilder UseOnDiskS3<TScenarioBuilder>(
-            this TScenarioBuilder scenarioBuilder,
-            string? root = null,
-            bool quietMode = false)
-            where TScenarioBuilder : IScenarioBuilder
-            => (TScenarioBuilder)scenarioBuilder.Use(
-                services => services.AddSingleton(
-                    _ => Fs3.AWS.FakeS3.CreateFileClient(
-                        root ?? Path.Join(Path.GetTempPath(), Path.GetRandomFileName()), quietMode)));
+        [ScenarioDependency]
+        public static void UseOnDiskS3(IServiceCollection services, string? root = null, bool quietMode = false)
+            => services.AddSingleton(_ => Fs3.AWS.FakeS3.CreateFileClient(
+                root ?? Path.Join(Path.GetTempPath(), Path.GetRandomFileName()), quietMode));
 
-        public static TScenarioBuilder UseInMemoryS3<TScenarioBuilder>(
-            this TScenarioBuilder scenarioBuilder)
-            where TScenarioBuilder : IScenarioBuilder
-            => (TScenarioBuilder)scenarioBuilder.Use(
-                services => services.AddSingleton(_ => Fs3.AWS.FakeS3.CreateMemoryClient()));
+        [ScenarioDependency]
+        public static void UseInMemoryS3(IServiceCollection services)
+            => services.AddSingleton(_ => Fs3.AWS.FakeS3.CreateMemoryClient());
 
-        public static TScenarioBuilder UseFileStoreBuckets<TScenarioBuilder>(
-            this TScenarioBuilder scenarioBuilder,
-            string root,
-            bool quietMode = false)
-            where TScenarioBuilder : IScenarioBuilder
-            => (TScenarioBuilder)scenarioBuilder.Use(
-                services => services.AddSingleton<Fs3.IBucketStore>(_ => new Fs3.FileStore(root, quietMode)));
-        
-        public static TScenarioBuilder UseMemoryStoreBuckets<TScenarioBuilder>(
-            this TScenarioBuilder scenarioBuilder)
-            where TScenarioBuilder : IScenarioBuilder
-            => (TScenarioBuilder)scenarioBuilder.Use(
-                services => services.AddSingleton<Fs3.IBucketStore>(_ => new Fs3.MemoryStore()));
+        [ScenarioDependency]
+        public static void UseFileStoreBuckets(IServiceCollection services, string root, bool quietMode = false)
+            => services.AddSingleton<Fs3.IBucketStore>(_ => new Fs3.FileStore(root, quietMode));
+
+        [ScenarioDependency]
+        public static void UseFileStoreBuckets(IServiceCollection services)
+            => services.AddSingleton<Fs3.IBucketStore>(_ => new Fs3.MemoryStore());
     }
 }
